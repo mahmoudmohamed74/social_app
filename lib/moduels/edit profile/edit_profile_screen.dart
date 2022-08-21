cf7fbdf86680283d1b263878b102c54bf3b3c759
mahmoudvolt <mahmodhoda328@gmail.com>
2022-08-21 05:26:27 +0200

    FireBase to edit Profile&Cover

---
+++ lib/moduels/edit profile/edit_profile_screen.dart
@@ -0,0 +1,252 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_app/cubit/cubit.dart';
+import 'package:social_app/cubit/states.dart';
+import 'package:social_app/shared/components/components/components.dart';
+import 'package:social_app/shared/styles/icon_broken.dart';
+
+class EditProfileScreen extends StatelessWidget {
+  var nameController = TextEditingController();
+  var bioController = TextEditingController();
+  var phoneController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SocialCubit, SocialState>(
+      listener: (context, state) {},
+      builder: (context, state) {
+        var userModel = SocialCubit.get(context).userModel;
+        var profileImage = SocialCubit.get(context).profileImage;
+        var coverImage = SocialCubit.get(context).coverImage;
+        nameController.text = userModel!.name!;
+        phoneController.text = userModel.phone!;
+        bioController.text = userModel.bio!;
+
+        return Scaffold(
+          appBar: defaultAppBar(
+            context: context,
+            title: "Edit profile",
+            actions: [
+              defaultTextButton(
+                function: () {
+                  SocialCubit.get(context).updateUser(
+                    name: nameController.text,
+                    phone: phoneController.text,
+                    bio: bioController.text,
+                  );
+                },
+                text: "Update",
+              ),
+              SizedBox(
+                width: 15,
+              ),
+            ],
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  if (state is UpdateUserDataLoadingState)
+                    LinearProgressIndicator(),
+                  if (state is UpdateUserDataLoadingState)
+                    SizedBox(
+                      height: 10,
+                    ),
+                  Container(
+                    height: 190.0,
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      children: [
+                        Align(
+                          child: Stack(
+                            alignment: AlignmentDirectional.topEnd,
+                            children: [
+                              Container(
+                                height: 140.0,
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(
+                                      4.0,
+                                    ),
+                                    topRight: Radius.circular(
+                                      4.0,
+                                    ),
+                                  ),
+                                  image: DecorationImage(
+                                    image: coverImage == null
+                                        ? NetworkImage(
+                                            '${userModel.cover}',
+                                          )
+                                        : FileImage(coverImage)
+                                            as ImageProvider,
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  SocialCubit.get(context).getCoverImage();
+                                },
+                                icon: CircleAvatar(
+                                  radius: 20,
+                                  child: Icon(
+                                    IconBroken.Camera,
+                                    size: 16,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          alignment: AlignmentDirectional.topCenter,
+                        ),
+                        Stack(
+                          alignment: AlignmentDirectional.bottomEnd,
+                          children: [
+                            CircleAvatar(
+                              radius: 64.0,
+                              backgroundColor:
+                                  Theme.of(context).scaffoldBackgroundColor,
+                              child: CircleAvatar(
+                                radius: 60,
+                                backgroundImage: profileImage == null
+                                    ? NetworkImage(
+                                        '${userModel.image}',
+                                      )
+                                    : FileImage(profileImage) as ImageProvider,
+                              ),
+                            ),
+                            IconButton(
+                              onPressed: () {
+                                SocialCubit.get(context).getProfileImage();
+                              },
+                              icon: CircleAvatar(
+                                radius: 20,
+                                child: Icon(
+                                  IconBroken.Camera,
+                                  size: 16,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  if (SocialCubit.get(context).profileImage != null ||
+                      SocialCubit.get(context).coverImage != null)
+                    Row(
+                      children: [
+                        if (SocialCubit.get(context).profileImage != null)
+                          Expanded(
+                            child: Column(
+                              children: [
+                                defaultButton(
+                                  function: () {
+                                    SocialCubit.get(context).uploadProfileImage(
+                                      name: nameController.text,
+                                      phone: phoneController.text,
+                                      bio: bioController.text,
+                                    );
+                                  },
+                                  text: "update profile",
+                                ),
+                                if (state is UpdateUserDataLoadingState)
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                if (state is UpdateUserDataLoadingState)
+                                  LinearProgressIndicator(),
+                              ],
+                            ),
+                          ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        if (SocialCubit.get(context).coverImage != null)
+                          Expanded(
+                            child: Column(
+                              children: [
+                                defaultButton(
+                                  function: () {
+                                    SocialCubit.get(context).uploadCoverImage(
+                                      name: nameController.text,
+                                      phone: phoneController.text,
+                                      bio: bioController.text,
+                                    );
+                                  },
+                                  text: "update cover",
+                                ),
+                                if (state is UpdateUserDataLoadingState)
+                                  SizedBox(
+                                    height: 5.0,
+                                  ),
+                                if (state is UpdateUserDataLoadingState)
+                                  LinearProgressIndicator(),
+                              ],
+                            ),
+                          ),
+                      ],
+                    ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  defaultFormField(
+                    controller: nameController,
+                    type: TextInputType.name,
+                    validate: (value) {
+                      if (value!.isEmpty) {
+                        return "name must not be empty";
+                      }
+                      return null;
+                    },
+                    lable: "Name",
+                    prefix: IconBroken.User,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  defaultFormField(
+                    controller: bioController,
+                    type: TextInputType.text,
+                    validate: (value) {
+                      if (value!.isEmpty) {
+                        return "bio must not be empty";
+                      }
+                      return null;
+                    },
+                    lable: "Bio",
+                    prefix: IconBroken.Info_Circle,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  defaultFormField(
+                    controller: phoneController,
+                    type: TextInputType.name,
+                    validate: (value) {
+                      if (value!.isEmpty) {
+                        return "please add your phone number";
+                      }
+                      return null;
+                    },
+                    lable: "Phone",
+                    prefix: IconBroken.Call,
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
